0690e324f67fc772ea27b67096ff89f4cb869e51
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-12-21 17:03:32 +0000

    Add API docstrings

--- src/papermill_service/app.py
+++ src/papermill_service/app.py
@@ -16,11 +16,13 @@ app = FastAPI()
 
 @app.get("/version")
 async def root():
+    """Get the running version of papermill_service."""
     return dict(version=__version__)
 
 
 @app.get("/params")
 async def params(settings: Settings = Depends(get_settings)):
+    """Get the arguments expected by the hosted notebook."""
     return inspect_notebook(settings.notebook_path)
 
 
@@ -30,6 +32,7 @@ async def start(
     parameters: dict = Body(default=dict()),
     settings: Settings = Depends(get_settings),
 ):
+    """Start an execution of the hosted notebook with the provided parameters."""
     task_id = uuid4()
     background_tasks.add_task(
         execute_notebook_with_tracking, task_id, settings.notebook_path, parameters
@@ -39,4 +42,5 @@ async def start(
 
 @app.get("/fetch/{task_id}")
 async def fetch(task_id: UUID):
+    """Fetch the result of a previously started execution."""
     return get_execution_status(task_id).status
